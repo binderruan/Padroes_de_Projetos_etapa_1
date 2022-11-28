183877816a8e5920f3e2e6095849301ad4d9cf22
Ruan Binder <105131152+ruanbinder@users.noreply.github.com>
2022-11-28 16:43:07 -0300

    Delete src directory

--- src/nullObject/AbstractLivros.java
+++
@@ -1,8 +0,0 @@
-package nullObject;
-
-//metodo que não tem implentção pois é uma classe abstrata
-public abstract class AbstractLivros {
-	protected String titulo;
-	
-	public abstract String getTitulo();
-}
--- src/observerPattern/Observer.java
+++
@@ -1,7 +0,0 @@
-package observerPattern;
-
-//interface observadora 
-public interface Observer {
-	// metodo notify recebe  objeto observavel e uma mensagem 
-    public void notify(Observable observable, String message); // quem mandou mensagem e quem recebe e mensagem
-}
